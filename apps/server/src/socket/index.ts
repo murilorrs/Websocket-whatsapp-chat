f172feeee184397175d7a705d9078ce6b60b3f4f
murilorrs <murilorodriguesrrs@gmail.com>
2025-06-01 01:51:17 -0300

    refact: add profilePicture and improve socket server

--- apps/server/src/socket/index.ts
+++ apps/server/src/socket/index.ts
@@ -1,42 +1,52 @@
 import { Server } from 'http';
 import { WebSocket, WebSocketServer } from 'ws';
 
-const users = new Map<string, WebSocket>();
+type User = {
+  id: string;
+  name: string;
+  profilePicture: string;
+};
+
+const users = new Map<User, WebSocket>();
 
 export function setupWebSocket(server: Server) {
   const wss = new WebSocketServer({ server });
 
-  wss.on('connection', (ws: WebSocket) => {
-    console.log('new websocket connection:');
+  wss.on('connection', (ws) => {
     ws.send('👋Connected');
 
-    ws.on('message', (message: any) => {
-      let userName;
+    ws.on('message', async (message) => {
       const data = JSON.parse(message.toString());
-      console.log('data', data);
+      console.log('New connection:', data.name);
+      console.log('Connections:', wss.clients.size);
 
       if (data.type === 'join' && typeof data.name === 'string') {
-        userName = data.name;
-        users.set(userName, ws);
+        const user = {
+          id: crypto.randomUUID(),
+          name: data.name,
+          profilePicture: await getProfilePicture(),
+        };
+        users.set(user, ws);
+        broadcastUsers();
       }
 
-      broadcastUsers();
-
-      wss.clients.forEach((client: any) => {
-        if (client.readyState === ws.OPEN) {
-          client.send(message.toString());
-        }
-      });
+      // wss.clients.forEach((client) => {
+      //   if (client.readyState === ws.OPEN) {
+      //     client.send(message.toString());
+      //   }
+      // });
     });
 
     ws.on('close', () => {
-      users.forEach((sock, name) => {
-        if (sock === ws) users.delete(name);
+      users.forEach((sock, user) => {
+        if (sock === ws) users.delete(user);
       });
       broadcastUsers();
     });
   });
 }
+
+// TODO: Quando desconectar no front dar reload na página
 
 function broadcastUsers() {
   const names = Array.from(users.keys());
@@ -45,4 +55,15 @@ function broadcastUsers() {
   users.forEach((user) => {
     user.send(payload);
   });
+}
+
+async function getProfilePicture() {
+  const response = await fetch(
+    'https://api.thecatapi.com/v1/images/search?mime_types=jpg,png',
+  );
+
+  const data = await response.json();
+  const profilePictureUrl = data[0].url;
+  console.log('Profile Picture URL:', profilePictureUrl);
+  return profilePictureUrl;
 }
