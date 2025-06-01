7188f9e41db1cd211bb11dd1f270ee30d3fb05fd
murilorrs <murilorodriguesrrs@gmail.com>
2025-06-01 00:13:25 -0300

    refact: improve WebSocket connection handling and user management

--- apps/server/src/socket/index.ts
+++ apps/server/src/socket/index.ts
@@ -1,14 +1,26 @@
 import { Server } from 'http';
 import { WebSocket, WebSocketServer } from 'ws';
 
+const users = new Map<string, WebSocket>();
+
 export function setupWebSocket(server: Server) {
   const wss = new WebSocketServer({ server });
 
-  wss.on('connection', (ws: any) => {
-    console.log('🔌 Nova conexão WebSocket');
+  wss.on('connection', (ws: WebSocket) => {
+    console.log('new websocket connection:');
+    ws.send('👋Connected');
 
     ws.on('message', (message: any) => {
-      console.log('📩 Mensagem recebida:', message.toString());
+      let userName;
+      const data = JSON.parse(message.toString());
+      console.log('data', data);
+
+      if (data.type === 'join' && typeof data.name === 'string') {
+        userName = data.name;
+        users.set(userName, ws);
+      }
+
+      broadcastUsers();
 
       wss.clients.forEach((client: any) => {
         if (client.readyState === ws.OPEN) {
@@ -17,6 +29,20 @@ export function setupWebSocket(server: Server) {
       });
     });
 
-    ws.send('👋 Conectado ao servidor WebSocket!');
+    ws.on('close', () => {
+      users.forEach((sock, name) => {
+        if (sock === ws) users.delete(name);
+      });
+      broadcastUsers();
+    });
+  });
+}
+
+function broadcastUsers() {
+  const names = Array.from(users.keys());
+  const payload = JSON.stringify({ type: 'user', users: names });
+
+  users.forEach((user) => {
+    user.send(payload);
   });
 }
